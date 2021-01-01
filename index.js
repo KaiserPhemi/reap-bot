66981d2d2e12b276c942fd375f38f6b173981bd3
KaiserPhemi <kaiser.phemi@protonmail.ch>
2021-01-01 19:52:31 +0100

    chore();
    - install dependencies

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // third-party libraries
 const puppeteer = require("puppeteer");
 
+const ps5Url = process.env.EBAY_PS5;
+
+console.log("the url", ps5Url);
+
 // initialize app
 (async () => {
   const browser = await puppeteer.launch();
