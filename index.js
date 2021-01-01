4884c6c2746e524cb5a25a19c37fa3c53a4dcf2b
KaiserPhemi <kaiser.phemi@protonmail.ch>
2021-01-01 19:52:31 +0100

    chore();
    - install dependencies

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 // third-party libraries
+require("dotenv").config();
 const puppeteer = require("puppeteer");
 
-// initialize app
-(async () => {
+const ps5Url = process.env.EBAY_PS5;
+const xBoxUrl = process.env.EBAY_XBOX;
+
+const scrapper = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto("https://example.com");
-  await page.screenshot({
-    path: "main-example.png",
-  });
-})();
+  await page.goto(xBoxUrl);
+  // await page.waitFor(1000);
+  // browser.close();
+  // await page.screenshot({
+  //   path: "main-example.png",
+  // });
+};
+
+scrapper().then((res) => {
+  console.log("Scrapper working", res);
+});
